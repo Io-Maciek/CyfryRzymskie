11b6dbfe7f9374be4b7c5e9046bcccfb2604be4d
Maciej Kuchcik <mkuchcik@gmail.com>
2022-02-02 10:50:11 +0100

    Dodaj pliki projektów.

---
+++ IntToRomanNumber.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyfryRzymskie
+{
+    public class IntToRomanNumber
+    {
+        public Dictionary<int, string> rzymskie { get; set; }
+
+
+        public IntToRomanNumber()
+        {
+            rzymskie = new Dictionary<int, string>();
+
+            rzymskie.Add(1, "I");
+            rzymskie.Add(4, "IV");
+            rzymskie.Add(5, "V");
+            rzymskie.Add(9, "IX");
+            rzymskie.Add(10, "X");
+            rzymskie.Add(40, "XL");
+            rzymskie.Add(50, "L");
+            rzymskie.Add(90, "XC");
+            rzymskie.Add(100, "C");
+            rzymskie.Add(400, "CD");
+            rzymskie.Add(500, "D");
+            rzymskie.Add(900, "CM");
+            rzymskie.Add(1000, "M");
+        }
+
+        public string ConvertToRoman(int number)
+        {
+            string wynik = "";
+
+            // txtRzymska.Text = ((Rzymska)liczba).ToString()+"   "+(int)((Rzymska)liczba);
+            int zerowanie = number;
+
+            //Console.WriteLine(zerowanie);
+
+            while (zerowanie > 0)
+            {
+                KeyValuePair<int,string> keyValuePair = rzymskie.Where(k => k.Key <= zerowanie).OrderByDescending(n => n.Key).First();
+
+                zerowanie -= keyValuePair.Key;
+
+                wynik += keyValuePair.Value;
+            }
+
+
+            return wynik;
+        }
+
+        public string ConvertToNumber(string roman)
+        {
+            int ret = 0;
+
+
+            for (int i = 0; i < roman.Length; i++)
+            {
+                try {
+                    char next = roman[i + 1];
+                    char pre = roman[i];
+
+                    var N = rzymskie.Where(n => n.Value == next.ToString()).First();
+                    var P = rzymskie.Where(n => n.Value == pre.ToString()).First();
+
+                    if (N.Key > P.Key)
+                    {
+                        ret -= P.Key;
+                    }
+                    else
+                    {
+                        ret += P.Key;
+                    }
+                }
+                catch (System.IndexOutOfRangeException)
+                {
+                    ret += rzymskie.Where(n => n.Value == roman[i].ToString()).First().Key;
+                }
+            }
+
+
+
+
+            return ret.ToString();
+        }
+
+        public string[] Chars()
+        {
+            return rzymskie.Values.ToArray();
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CyfryRzymskie
+{
+    public partial class MainWindow : Window
+    {
+        IntToRomanNumber con = new IntToRomanNumber();
+
+        bool buttonPressed = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> r = new List<string>();
+            for (int i = 1; i <= 100; i++)
+            {
+                r.Add(con.ConvertToRoman(i));
+            }
+
+            Console.WriteLine(r.OrderByDescending(n=>n.Length).First());
+
+        }
+
+        private void txtCyfra_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!buttonPressed)
+            {
+                int liczba;
+                if (int.TryParse(txtCyfra.Text, out liczba))
+                {
+                    if (liczba >= 0)
+                        txtRzymska.Text = con.ConvertToRoman(liczba);
+                    else
+                    {
+                        txtRzymska.Text = "-" + con.ConvertToRoman(Math.Abs(liczba));
+                    }
+                }
+                else if (txtCyfra.Text == "")
+                {
+                    txtRzymska.Text = "";
+                }
+                else
+                {
+                    txtRzymska.Text = "Błąd";
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Random rn = new Random();
+
+            if (buttonPressed == true)
+            {
+                txtRzymska.IsEnabled = false;
+                txtCyfra.IsEnabled = true;
+                lblFirst.Visibility = Visibility.Visible;
+                lblSecond.Visibility = Visibility.Hidden;
+
+                txtCyfra.Text = "";
+                txtRzymska.Text = "";
+
+
+                buttonPressed = false;
+                //txtCyfra.Text = rn.Next(1, 85).ToString();
+            }
+            else
+            {
+                txtCyfra.IsEnabled = false;
+                txtRzymska.IsEnabled = true;
+                lblSecond.Visibility = Visibility.Visible;
+                lblFirst.Visibility = Visibility.Hidden;
+
+
+                txtRzymska.Text = "";
+                txtCyfra.Text = "";
+                //txtCyfra.Text = rn.Next(1, 85).ToString();
+
+                buttonPressed = true;
+            }
+
+
+        }
+
+        private void txtRzymska_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (buttonPressed)
+            {
+                txtRzymska.Text = new string(txtRzymska.Text.Where(n => con.Chars().Contains(n.ToString().ToUpper())).ToArray()).ToUpper();
+                txtRzymska.Select(txtRzymska.Text.Length, 0);
+
+
+                txtCyfra.Text = con.ConvertToNumber(txtRzymska.Text.ToUpper().Trim());
+            }
+        }
+    }
+}
